ee93867bf24241a7484ce547bf525b7e335be38a
GRaynaud <gaetan.raynaud@polytechnique.edu>
2019-11-12 20:12:27 -0500

    update

--- Vibrations_Forcees.py
+++ Vibrations_Forcees.py
@@ -18,16 +18,18 @@ dx = L/Nx
 alpha0 = 0.3 #rad, angle d'attaque vertical
 v = 1 #m/s vitesse du pieton
 dpas = 0.5 #m taille d'un pas
+mg = 1e3 #N poids du marcheur
 
 # Description des variables temporelles
 
-Tmax = 1
-dt = 0.01
+Tmax = 10
+dt = 0.005
 Nt = int(Tmax/dt)
+t = np.linspace(0,Tmax,Nt)
 
 # Description des variables dynamiques
 
-EI = 1e3
+EI = 1e4
 rhoA = 1e0
 
 # Conditions initiales
@@ -59,10 +61,39 @@ y = np.zeros((Nt,Nx))
 y[0,:] = y0
 y[1,:] = y0 + dt*dty0
 
+
+# definition de l'excitation
+
+# Excitation discontinue --> Cas 2
+def f2(xpos,tpos):
+    # on remplace le dirac par une gaussienne de variance tres faible
+    sigma = dx
+    fvalue = -1.*mg*np.cos(alpha0*(2*(v*tpos/dpas - np.floor(v*tpos/dpas))-1))
+    fvalue *= 1./(sigma*np.sqrt(2*np.pi)) * np.exp(-(xpos-dpas*np.floor(v*tpos/dpas))**2/(2*sigma**2))
+    return fvalue
+
+# Excitation continue --> Cas 1
+def f1(xpos,tpos):
+    # on remplace le dirac par une gaussienne de variance tres faible
+    sigma = dx
+    fvalue = -1.*mg
+    fvalue *= 0.5*(1+np.cos(alpha0)) + 0.5*(1-np.cos(alpha0))*np.cos(v*tpos/dpas)
+    fvalue *= 1./(sigma*np.sqrt(2*np.pi)) * np.exp(-(xpos-v*tpos)**2/(2*sigma**2))
+    return fvalue
+
+
 # Intégration en temps
 
+'''
+(Id + EI*dt^2/2rhoA * A) y^k+1 = 2*y^k - y^k-1 - EI*dt^2/2rhoA * A*y^k + dt^2/rhoA * f(t)
+
+A : operateur de derivation en espace
+f(t) : vecteur dont les coordonnées sont f(x,t) --> excitation. Choisir au choix f1 ou f2
+
+'''
+
 for i in range(2,Nt):
-    X = 2.*y[i-1,:] - y[i-2,:] - 0.5*(dt**2)*(EI/rhoA)*np.dot(A,y[i-1,:])
+    X = 2.*y[i-1,:] - y[i-2,:] - 0.5*(dt**2)*(EI/rhoA)*np.dot(A,y[i-1,:]) + (dt)**2/rhoA * f1(x,i*dt)
     y[i,:] = np.dot(invB,X)
 
 # Plot
@@ -71,15 +102,16 @@ for i in range(2,Nt):
 # Plot
 
 
-plt.figure()
-for i in range(Nt):
-    if i%10 == 0:
-        plt.plot(x,y[i,:], label=str(i))
-plt.xlabel('Horizontal axis x')
-plt.ylabel('Deflection (m)')
-plt.legend()
-plt.show()
+#plt.figure()
+#for i in range(Nt):
+#    if i%10 == 0:
+#        plt.plot(x,y[i,:], label=str(i))
+#plt.xlabel('Horizontal axis x')
+#plt.ylabel('Deflection (m)')
+#plt.legend()
+#plt.show()
 
+# Plot du midspan deflection
     
 plt.figure()
 plt.plot(t,y[:,int(0.5*Nx)])
