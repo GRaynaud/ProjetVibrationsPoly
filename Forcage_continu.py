2b3bc524e6c8fcfe5c0da2d991c8189c2db3ffd6
GRaynaud <gaetan.raynaud@polytechnique.edu>
2019-11-22 15:09:57 -0500

    Update Forcage_continu.py

--- Forcage_continu.py
+++ Forcage_continu.py
@@ -174,10 +174,11 @@ def plot_evolution():
         plt.plot(tab_x,1e3*tab_y)
         plt.scatter(x_pieton,1e3*y_pieton)
         plt.xlabel('x (m)')
-        plt.ylabel('y ($\times 10^{-3}$m)')
-        plt.title('$t=$%.2e' %t)
+        plt.ylabel('y ($\\times 10^{-3}$m)')
+        plt.title('$t=$%s s' %round(t,2))
         axes = plt.gca()
         axes.set_ylim([-ymaj,ymaj])
+        plt.tight_layout()
         plt.pause(dt)
         plt.savefig('File_evolution_%03d.png' % i)
 
